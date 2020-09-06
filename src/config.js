1b993f2b1b7fc1120770bb69c68628d3691b242b
Chayce Knaub <chknaub@yahoo.com>
2020-09-06 15:05:15 -0400

    ready to deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  API_ENDPOINT: "http://localhost:8000/api",
-  //API_ENDPOINT: "https://shower-thoughts-api.herokuapp.com/api",
+  //API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://shower-thoughts-api.herokuapp.com/api",
 };
